cae04b03d307f6893bb6b3b58cf3864a8659ce6a
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-14 16:52:30 +0530

    adding albumentations libraries

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #  This is the data processing part
-#  we are going to load the data and split it and data augmentation on the training part 
+#  we are going to load the data and split it and data augmentation on the training part
 #  and leave the validation as it is
 
 #  importing the libraries
@@ -7,18 +7,26 @@
 import os
 import numpy as np
 import cv2 as cv
+
 # glob is used to extract the path (here images)
 from glob import glob
+
 # for progress bar as the progress is not visible for us(how much are done and how much are left)
 from tqdm import tqdm
+
 # Scikit-learn (Sklearn) is the most useful and robust library for machine learning in Python
 #  More Info : https://www.tutorialspoint.com/scikit_learn/scikit_learn_introduction.htm#:~:text=Scikit%2Dlearn%20(Sklearn)%20is,a%20consistence%20interface%20in%20Python.
 from sklearn.model_selection import train_test_split
-
+# for augmentation using the augmentation library below
+#  functions stated after the import keyword
+# More Info: https://www.datacamp.com/tutorial/complete-guide-data-augmentation
+from albumentations import HorizontalFlip, GridDistortion, OpticalDistortion, ChannelShuffle, CoarseDropout, CenterCrop, Crop, Rotate
 
 """ Creating a directory """
+
+
 #  creating a function for creating directory
-# for creating a directory by itself 
+# for creating a directory by itself
 # using a os module
 def create_directory(path):
     #  checking if the path exist if not create the path
@@ -27,24 +35,25 @@ def create_directory(path):
 
 
 """Load Data function"""
+
+
 # passing the path and argument split as 0.1
 # 0.1 means out of the complete data we will be using the 1% of the data for the testing and validation purpose
-# here the testing and the validation split is the same 
-def loading_data(path,split=0.1):
-    """ Loading images and masks """
+# here the testing and the validation split is the same
+def loading_data(path, split=0.1):
+    """Loading images and masks"""
     # loading the Images by refering them with the variable X
     # loading the Masks by refering them with the variable Y
-    # using the glob variable 
+    # using the glob variable
     # repective images and masks while zip() are not their respective ones so we need to sequence them
     """ Sorting """
     #  i.e why the "sorted" function is used
     # load all the images ending with the jpg extention For images
     """ For images """
-    X_image =  sorted(glob(os.path.join(path,"images","*.jpg")))
+    X_image = sorted(glob(os.path.join(path, "images", "*.jpg")))
     #  load all the images ending with the jpg extention For masks
     """ For maks """
-    Y_mask =  sorted(glob(os.path.join(path,"masks","*.png")))
-
+    Y_mask = sorted(glob(os.path.join(path, "masks", "*.png")))
 
     """Only for Chacking the image and masks are their corresponding pairs"""
     # #  basically X and Y are lists
@@ -61,7 +70,7 @@ def loading_data(path,split=0.1):
     #     #  read and save masks here
     #     y = cv.imread(y)
     #     cv.imwrite("y.png",y*255) # making the mask white for checking
-    
+
     #     break
 
     """splitting the data into testing and training"""
@@ -71,27 +80,52 @@ def loading_data(path,split=0.1):
     # splitting the image and masks
     # providing X and Y ,basically the list of all images
     # make sure the random state is same or else it would not be split the same
-    train_x, test_x = train_test_split(X_image, test_size=spliting_size,random_state=42)
-    train_y, test_y = train_test_split(Y_mask, test_size=spliting_size,random_state=42)
+    train_x, test_x = train_test_split(
+        X_image, test_size=spliting_size, random_state=42
+    )
+    train_y, test_y = train_test_split(Y_mask, test_size=spliting_size, random_state=42)
 
     #  returning the training and testing set
-    return (train_x,test_x),(train_y,test_y)
+    return (train_x, test_x), (train_y, test_y)
 
 
-def augment_data(images ,masks,save_path,augment=True):
+def augment_data(images, masks, save_path, augment=True):
     # setting the item height and wifth , we'll set it because fixed data needed (size)
     H = 512
     W = 512
-    
+
     #  loop over the images
     # tqdm used for the progress bar, one variable "total"how much the loop would run
     #  for saving the images we need a name
-    #  splitting it using the slash 
-    for x,y in tqdm(zip(images, masks), total=len(images)):
+    #  splitting it using the slash
+    for x, y in tqdm(zip(images, masks), total=len(images)):
         # print(x,y)
         # break
+        """Extract the name"""
         name = x.split("\\")[-1].split(".")[0]
-        print(name)
+        # print(name)
+
+        """Read the image and mask"""
+        #  reading the image as rgb
+        x = cv.imread(x, cv.IMREAD_COLOR)
+        #  reading the mask as rgb
+        y = cv.imread(y, cv.IMREAD_COLOR)
+
+        #  after loading the library
+        if augment == True:
+            #  if True something will definitely happn and incrase the list with more array of images
+            pass
+        else:
+            # If False , then we resize the original image and save them
+            X = [x]
+            Y = [y]
+
+        #  looping them
+        for i_image,m_mask in zip(X,Y):
+            
+
+
+
         break
 
 
@@ -99,8 +133,8 @@ def augment_data(images ,masks,save_path,augment=True):
 # The __name__ variable merely holds the name of the module or script unless the current module is executing,
 # __main__ is the name of the environment where top-level code is run.
 #  “Top-level code” is the first user-specified Python module that starts running.
-if __name__=="__main__":
-    """ Seeding the environment """
+if __name__ == "__main__":
+    """Seeding the environment"""
     # the randomness present between the numpy , we will seed it so that i have the same randomness every time
     # More Info: https://medium.com/geekculture/the-story-behind-random-seed-42-in-machine-learning-b838c4ac290a#:~:text=seed()%20%3F-,random.,generator%20with%20the%20given%20value.
     np.random.seed(42)
@@ -110,26 +144,25 @@ if __name__=="__main__":
     #  path for the data set (below)
     data_path = "people_segmentation"
     # function load data, this function will take the data path and returns the training and testing the masks and images
-    (train_x,test_x),(train_y,test_y) = loading_data(data_path)
+    (train_x, test_x), (train_y, test_y) = loading_data(data_path)
 
     """ Checking the length of the training and testing"""
 
     print(f"Train:\t{len(train_x)} - {len(train_y)}")
     print(f"Train:\t{len(test_x)} - {len(test_y)}")
 
-
     """ Creating directories to save the augmeneted data for images and masks seperatly"""
     create_directory("new_data_generated/train/image/")
     create_directory("new_data_generated/train/mask/")
     create_directory("new_data_generated/test/image/")
     create_directory("new_data_generated/test/mask/")
-    
+
     """ Applying the data augmentation for training"""
     # on testing we wont appy
-    #  creating a function for augmenting 
-    #  first it would take the images either for the training or for the mask 
+    #  creating a function for augmenting
+    #  first it would take the images either for the training or for the mask
     # then it would take the path where thr images is needed to be saved
-    # last a booblean variable cld augment, 
+    # last a booblean variable cld augment,
     # if it is true then appy augmnetation if false dont apply
 
-    augment_data(train_x,train_y,"new_data/train/", augment=True)
+    augment_data(train_x, train_y, "new_data/train/", augment=True)
